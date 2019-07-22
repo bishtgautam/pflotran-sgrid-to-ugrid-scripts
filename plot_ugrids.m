ce499c70a7a886bda7f253685b9561ad1489af69
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-07-22 08:24:00 -0700

    updates plotting of hex ugrid

--- plot_ugrids.m
+++ plot_ugrids.m
@@ -5,21 +5,31 @@ vertices = h5read(h5_ugrid_filename,'/Domain/Vertices')';
 
 figure;
 for rr = 1:length(region_info.Groups)
-    info = h5info(h5_ugrid_filename,region_info.Groups(rr).Name);
-    cids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name]);
-    
-    if (~strcmp(info.Datasets(1).Name,'Vertex Ids'))
-        error(['For ' region_info.Groups(rr).Name ': "Vertex Ids" not found']);
-    end
-    
-    vids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name])';
-    
-    for k = 1:size(vids,1)
-        xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
-        plot3(xx,yy,zz,'-rs','linewidth',2); hold all;
+    if (~strcmp(region_info.Groups(rr).Name,'/Regions/Top'))
+        info = h5info(h5_ugrid_filename,region_info.Groups(rr).Name);
+        cids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name]);
+
+        if (~strcmp(info.Datasets(1).Name,'Vertex Ids'))
+            error(['For ' region_info.Groups(rr).Name ': "Vertex Ids" not found']);
+        end
+
+        vids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name])';
+
+        for k = 1:size(vids,1)
+            xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+            plot3(xx,yy,zz,'-rs','linewidth',2); hold all;
+        end
     end
 end
 
+vids = h5read(h5_ugrid_filename,'/Regions/Top/Vertex Ids')';
+for k = 1:size(vids,1)
+    xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+    %plot3(xx,yy,zz,'-bs','linewidth',2); hold all;
+    fill3(xx,yy,zz,mean(zz)); hold all;
+end
+plot3(vertices(:,1),vertices(:,2),vertices(:,3),'.k')
+grid on
 
 h5_ugrid_filename = '/Users/bish218/projects_1drive/elm-pflotran-coupling/dataset/5km/pflotran/5km_prism_ugrid.h5';
 vertices = h5read(h5_ugrid_filename,'/Domain/Vertices')';
