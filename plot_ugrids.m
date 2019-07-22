d1cfb5c6211de840cd96d1d124e9f622897a1276
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-07-22 07:37:56 -0700

    plot the mesh

---
+++ plot_ugrids.m
@@ -0,0 +1,59 @@
+h5_ugrid_filename = '/Users/bish218/projects_1drive/elm-pflotran-coupling/dataset/5km/pflotran/5km_hex_ugrid.h5';
+region_info = h5info(h5_ugrid_filename,'/Regions');
+
+vertices = h5read(h5_ugrid_filename,'/Domain/Vertices')';
+
+figure;
+for rr = 1:length(region_info.Groups)
+    info = h5info(h5_ugrid_filename,region_info.Groups(rr).Name);
+    cids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name]);
+    
+    if (~strcmp(info.Datasets(1).Name,'Vertex Ids'))
+        error(['For ' region_info.Groups(rr).Name ': "Vertex Ids" not found']);
+    end
+    
+    vids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name])';
+    
+    for k = 1:size(vids,1)
+        xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+        plot3(xx,yy,zz,'-rs','linewidth',2); hold all;
+    end
+end
+
+
+h5_ugrid_filename = '/Users/bish218/projects_1drive/elm-pflotran-coupling/dataset/5km/pflotran/5km_prism_ugrid.h5';
+vertices = h5read(h5_ugrid_filename,'/Domain/Vertices')';
+cells = h5read(h5_ugrid_filename,'/Domain/Cells')';
+
+figure;
+vids = cells(end-nx*ny*4+1:end,5:7);
+for k = 1:size(vids,1)
+    xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+    plot3(xx,yy,zz,'-ro','linewidth',2); hold all;
+end
+
+figure;
+vids = cells(end-nx*ny*4+1:end,5:7);
+for k = 1:size(vids,1)
+    xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+    fill3(xx(1:3),yy(1:3),zz(1:3),mean(zz(1:3))); hold all;
+end
+colormap jet;
+
+
+region_info = h5info(h5_ugrid_filename,'/Regions');
+for rr = 1:length(region_info.Groups)
+    info = h5info(h5_ugrid_filename,region_info.Groups(rr).Name);
+    cids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name]);
+    
+    if (strcmp(info.Datasets(1).Name,'Vertex Ids'))
+        
+        vids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name])';
+        
+        for k = 1:size(vids,1)
+            xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
+            plot3(xx,yy,zz,'-ks','linewidth',2); hold all;
+        end
+    end
+end
+grid on;
