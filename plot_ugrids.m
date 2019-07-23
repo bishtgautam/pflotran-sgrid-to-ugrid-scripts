2d53843cc57064b908ec9a5631e1d40201cd1a9e
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-07-23 06:58:53 -0700

    minor changes to plotting scripts

--- plot_ugrids.m
+++ plot_ugrids.m
@@ -30,29 +30,35 @@ for k = 1:size(vids,1)
 end
 plot3(vertices(:,1),vertices(:,2),vertices(:,3),'.k')
 grid on
+zlim([0 800])
+view(-33,70)
+caxis([100 500])
+colorbar
+orient landscape
+print -dpdf hex_ugrid.pdf -fillpage
+
 
 h5_ugrid_filename = '/Users/bish218/projects_1drive/elm-pflotran-coupling/dataset/5km/pflotran/5km_prism_ugrid.h5';
 vertices = h5read(h5_ugrid_filename,'/Domain/Vertices')';
 cells = h5read(h5_ugrid_filename,'/Domain/Cells')';
 
 figure;
-vids = cells(end-nx*ny*4+1:end,5:7);
+vids = h5read(h5_ugrid_filename,'/Regions/Top/Vertex Ids')';
 for k = 1:size(vids,1)
     xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
-    plot3(xx,yy,zz,'-ro','linewidth',2); hold all;
+    plot3(xx,yy,zz,'-ko','linewidth',2); hold all;
 end
 
-figure;
-vids = cells(end-nx*ny*4+1:end,5:7);
+% figure;
+vids = h5read(h5_ugrid_filename,'/Regions/Top/Vertex Ids')';
 for k = 1:size(vids,1)
     xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
     fill3(xx(1:3),yy(1:3),zz(1:3),mean(zz(1:3))); hold all;
 end
-colormap jet;
-
 
 region_info = h5info(h5_ugrid_filename,'/Regions');
 for rr = 1:length(region_info.Groups)
+    if (~strcmp(region_info.Groups(rr).Name,'/Regions/Top'))
     info = h5info(h5_ugrid_filename,region_info.Groups(rr).Name);
     cids = h5read(h5_ugrid_filename,[region_info.Groups(rr).Name '/' info.Datasets(1).Name]);
     
@@ -62,8 +68,16 @@ for rr = 1:length(region_info.Groups)
         
         for k = 1:size(vids,1)
             xx = vertices([vids(k,:) vids(k,1) ],1); yy = vertices([vids(k,:) vids(k,1) ],2); zz = vertices([vids(k,:) vids(k,1) ],3);
-            plot3(xx,yy,zz,'-ks','linewidth',2); hold all;
+            plot3(xx,yy,zz,'-rs','linewidth',2); hold all;
         end
+    end
     end
 end
 grid on;
+zlim([0 800])
+view(-33,70)
+caxis([100 500])
+colorbar
+orient landscape
+print -dpdf prism_ugrid.pdf -fillpage
+
